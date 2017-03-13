20f92dc5ddac999c9cc165a7a2f36a1c154b0204
shimmer.bjh <shimmer.bjh@ip-v6.local>
2017-03-13 16:50:42 +0800

    fix bugs

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ChatRoom
-## 这是一个node写的聊天小程序，主要功能为设置用户名，消息发送接收，以及聊天室人数提醒.
+#  chat-room
+
+-----
+
+> * socket.io学习一下，写一个简单的聊天室
+> * 设计模式主要是订阅发布模式,通过不断地订阅发布来定义客户端和服务端的回调处理
+> * git clone https://github.com/Billshimmer/ChatRoom.git
+> * npm install --save
+> * npm start
---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = require("express")();
+var http = require('http').createServer(app);
+var io = require("socket.io")(http);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+io.on("connection", function (socket) {
+  console.log("a user connected");
+
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+  })
+})
+
+io.on("connection", function(socket){
+  socket.on("chat message", function (msg) {
+    console.log("服务端消息: " + msg);
+    io.sockets.emit('chat message', msg);
+  })
+})
+
+
+
+http.listen(2022, function () {
+  console.log("welcome to our chat room!");
+})
